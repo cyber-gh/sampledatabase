34101bd9d5ac8fb529e440df742986358c6b68e2
cyber-gh <solgheorghe@gmail.com>
2018-08-24 18:54:41 +0300

    Update logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -7,32 +7,32 @@
 #include "logger.h"
 
 bool LogCreated = false;
-//
-//void Log(char* message){
-//    FILE* file;
-//
-//    if (!LogCreated) {
-//        file = fopen(LOGFILE, "w");
-//        LogCreated = true;
-//    } else {
-//
-//        file = fopen(LOGFILE, "a");
-//    }
-//
-//    if (file == NULL){
-//        if (LogCreated){
-//            LogCreated = false;
-//        }
-//        return ;
-//    } else {
-//        fputs(message, file);
-//        fputs("\n", file);
-//    }
-//
-//    if (file){
-//        fclose(file);
-//    }
-//}
+
+void Log(char* message){
+   FILE* file;
+
+   if (!LogCreated) {
+       file = fopen(LOGFILE, "w");
+       LogCreated = true;
+   } else {
+
+       file = fopen(LOGFILE, "a");
+   }
+
+   if (file == NULL){
+       if (LogCreated){
+           LogCreated = false;
+       }
+       return ;
+   } else {
+       fputs(message, file);
+       fputs("\n", file);
+   }
+
+   if (file){
+       fclose(file);
+   }
+}
 
 void DeleteLog(){
     int res = remove(LOGFILE);
